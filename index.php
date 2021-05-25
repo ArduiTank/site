803cc53b5850507475720ab532c4c9bc724556da
Antonin De Breuck <antonindebreuck@gmail.com>
2021-05-25 10:48:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -42,6 +42,7 @@
 	<!-- Bulma Version 0.7.4-->
 	<link rel="stylesheet" href="https://unpkg.com/bulma@0.7.4/css/bulma.min.css" />
 	<link rel="stylesheet" href="https://unpkg.com/bulma-modal-fx/dist/css/modal-fx.min.css" />
+	
 	<!-- Bootstrap Version 5.0.0 beta 1 -->
 	<!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">-->
 
