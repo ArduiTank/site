6e71a055eaa07100646d314d893845df37535d8e
Antonin De Breuck <antonindebreuck@gmail.com>
2021-05-25 10:53:51 +0200

    Create footer.php

---
+++ include/footer.php
@@ -0,0 +1,12 @@
+		<div class="hero-foot">
+			<nav class="tabs is-boxed is-fullwidth">
+				<ul class="has-text-centered" style="border-bottom-style: none;">
+					<li>
+						Créé le 10/02/2020
+					</li>
+					<li>
+						Par AntoDB
+					</li>
+				</ul>
+			</nav>
+		</div>
