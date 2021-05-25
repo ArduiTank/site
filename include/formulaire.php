d562290603270e2258d21c3dc5b191304f3dc57e
Antonin De Breuck <antonindebreuck@gmail.com>
2021-05-25 14:47:29 +0200

    Update formulaire.php

--- include/formulaire.php
+++ include/formulaire.php
@@ -1,7 +1,7 @@
 <?php
 	$name = $_POST['name'];
 	$email = $_POST['email'];
-	if(isset($_POST["subject"]) == '') {
+	if(!isset($_POST["subject"]) == '') {
 		$subject = $_POST['subject'];
 	}
 	$message = $_POST['message'];
