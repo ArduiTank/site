a28b95c36e7e52a076bbfa432a75d4989080d75d
Antonin De Breuck <antonindebreuck@gmail.com>
2021-05-25 10:54:01 +0200

    Update index.php
    Header => Include

--- index.php
+++ index.php
@@ -1824,18 +1824,9 @@
 			</div>
 		</div>
 
-		<div class="hero-foot">
-			<nav class="tabs is-boxed is-fullwidth">
-				<ul class="has-text-centered" style="border-bottom-style: none;">
-					<li>
-						Créé le 10/02/2020
-					</li>
-					<li>
-						Par AntoDB
-					</li>
-				</ul>
-			</nav>
-		</div>
+		<?php
+			include("include/footer.php");
+		?>
 	</section>
 
 	<button onclick="topFunction()" id="scroll_to_top" title="Go to top">
