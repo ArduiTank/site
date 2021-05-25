da178942f39b398e2d813416a718dd1da1798a00
Antonin De Breuck <antonindebreuck@gmail.com>
2021-05-25 10:51:59 +0200

    Create header.php

---
+++ include/header.php
@@ -0,0 +1,272 @@
+        <div class="hero-head">
+			<nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation" id="navbarid" style="background-color: rgba(54, 54, 54, 1);">
+				<div class="container">
+					<div class="navbar-brand">
+						<a class="navbar-item" href="./">
+							<img src="./img/Logo V2.2 min.png" width="50" style="max-height: 100%;">
+						</a>
+			
+						<a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+							<span aria-hidden="true"></span>
+							<span aria-hidden="true"></span>
+							<span aria-hidden="true"></span>
+							<span aria-hidden="true"></span>
+							<span aria-hidden="true"></span>
+							<span aria-hidden="true"></span>
+							<span aria-hidden="true"></span>
+						</a>
+					</div>
+			
+					<div id="navbarBasicExample" class="navbar-menu">
+						<div class="navbar-start">
+							<a class="navbar-item" href="./">
+								Accueil
+							</a>
+							<a class="navbar-item" href="./#informations">
+								Informations
+							</a>
+							<a class="navbar-item" href="./#equipe">
+								Équipe
+							</a>
+							<div class="navbar-item has-dropdown is-hoverable" href="./#actu">
+								<a class="navbar-link">
+									Actualités
+								</a>		
+								<div class="navbar-dropdown">
+									<div class="navbar-item dropdown is-hoverable">
+										<a class="navbar-link dropdown-trigger is-fullwidth"  style="width: 100%;">
+											01/03/2021
+										</a>
+										<div class="dropdown-menu drop2">
+											<a class="navbar-item" href="./#01032021-4">
+												Impression 3D - Maintient tourelle et pièce amovible flancs et canon
+											</a>
+											<a class="navbar-item" href="./#01032021-3">
+												Impression 3D - Pièce tourelle sur l'engrenage
+											</a>
+											<a class="navbar-item" href="./#01032021-2">
+												"Hack" du servomoteur du tank 2
+											</a>
+											<a class="navbar-item" href="./#01032021-1">
+												Modélisation 3D - Tourelle
+											</a>
+										</div>
+									</div>
+									<div class="navbar-item dropdown is-hoverable">
+										<a class="navbar-link dropdown-trigger is-fullwidth"  style="width: 100%;">
+											28/02/2021
+										</a>
+										<div class="dropdown-menu drop2">
+											<a class="navbar-item" href="./#28022021-1">
+												Site - Navbar, actualités plus pratique
+											</a>
+										</div>
+									</div>
+									<div class="navbar-item dropdown is-hoverable">
+										<a class="navbar-link dropdown-trigger is-fullwidth"  style="width: 100%;">
+											27/02/2021
+										</a>
+										<div class="dropdown-menu drop2">
+											<a class="navbar-item" href="./#27022021-1">
+												Site - Navbar, actualités plus pratique
+											</a>
+										</div>
+									</div>
+									<div class="navbar-item dropdown is-hoverable">
+										<a class="navbar-link dropdown-trigger is-fullwidth"  style="width: 100%;">
+											26/02/2021
+										</a>
+										<div class="dropdown-menu drop2">
+											<a class="navbar-item" href="./#26022021-4">
+												Modélisation et impression 3D et CNC - Plaque de devant et ultrason
+											</a>
+											<a class="navbar-item" href="./#26022021-3">
+												PCB - Test avec plaque de cuivre
+											</a>
+											<a class="navbar-item" href="./#26022021-2">
+												Impression 3D - Bouton ON/OFF télécommande
+											</a>
+											<a class="navbar-item" href="./#26022021-1">
+												Site - Responsive (enfin)
+											</a>
+										</div>
+									</div>
+									<div class="navbar-item dropdown is-hoverable">
+										<a class="navbar-link dropdown-trigger is-fullwidth"  style="width: 100%;">
+											25/02/2021
+										</a>
+										<div class="dropdown-menu drop2">
+											<a class="navbar-item" href="./#25022021-1">
+												"Hack" du servomoteur
+											</a>
+										</div>
+									</div>
+									<div class="navbar-item dropdown is-hoverable">
+										<a class="navbar-link dropdown-trigger is-fullwidth"  style="width: 100%;">
+											24/02/2021
+										</a>
+										<div class="dropdown-menu drop2">
+											<a class="navbar-item" href="./#24022021-3">
+												Changement des moteurs
+											</a>
+											<a class="navbar-item" href="./#24022021-2">
+												CNC - Plaque tournante de la tourelle
+											</a>
+											<a class="navbar-item" href="./#24022021-1">
+												Arrivée des moteurs
+											</a>
+										</div>
+									</div>
+									<div class="navbar-item dropdown is-hoverable">
+										<a class="navbar-link dropdown-trigger is-fullwidth"  style="width: 100%;">
+											23/02/2021
+										</a>
+										<div class="dropdown-menu drop2">
+											<a class="navbar-item" href="./#23022021-4">
+												Impression 3D - Roue crantée pour le servo moteur (axe horizontale) & Support du roulement avec les fils
+											</a>
+											<a class="navbar-item" href="./#23022021-3">
+												CNC - Planches du dessus de la base du tank
+											</a>
+											<a class="navbar-item" href="./#23022021-2">
+												Test du microcontrôleur
+											</a>
+											<a class="navbar-item" href="./#23022021-1">
+												Impression 3D - Pièce d'attache des planches supérieures
+											</a>
+										</div>
+									</div>
+									
+									<div class="navbar-item dropdown is-hoverable">
+										<a class="navbar-link dropdown-trigger is-fullwidth"  style="width: 100%;">
+											22/02/2021
+										</a>
+										<div class="dropdown-menu drop2">
+											<a class="navbar-item" href="./#22022021-6">
+												Site - État actuel et animations
+											</a>
+											<a class="navbar-item" href="./#22022021-5">
+												Modélisation 3D - Pièce d'attache des trois planches du dessus et maintient de la planche arrière
+											</a>
+											<a class="navbar-item" href="./#22022021-4">
+												Changement de moteur & code
+											</a>
+											<a class="navbar-item" href="./#22022021-3">
+												CNC - Suite découpe des planches derrière le tank
+											</a>
+											<a class="navbar-item" href="./#22022021-2">
+												Modélisation 3D - Télécommande
+											</a>
+											<a class="navbar-item" href="./#22022021-1">
+												Impression 3D de la pièce d'attache pour les planches arrière du tank & découpe CNC des planches
+											</a>
+										</div>
+									</div>
+
+									<div class="navbar-item dropdown is-hoverable">
+										<a class="navbar-link dropdown-trigger is-fullwidth"  style="width: 100%;">
+											21/02/2021
+										</a>
+										<div class="dropdown-menu drop2">
+											<a class="dropdown-item" href="index.html#21022021-3">
+												Modélisation 3D - Pièce d'attache pour les planches arrière du tank
+											</a>
+											<a class="dropdown-item" href="index.html#21022021-2">
+												Modélisation 3D - Télécommande
+											</a>
+											<a class="dropdown-item" href="index.html#21022021-1">
+												Gravure de la planche du fond à la CNC
+											</a>
+										</div>
+									</div>
+
+									<div class="navbar-item dropdown is-hoverable">
+										<a class="navbar-link dropdown-trigger is-fullwidth"  style="width: 100%;">
+											20/02/2021
+										</a>
+										<div class="dropdown-menu drop2">
+											<a class="dropdown-item" href="index.html#20022021-2">
+												Chaîne et engrenage & CNC flancs du tank
+											</a>
+											<a class="dropdown-item" href="index.html#20022021-1">
+												Plan 2D, Télécommande
+											</a>
+										</div>
+									</div>
+
+									<div class="navbar-item dropdown is-hoverable">
+										<a class="navbar-link dropdown-trigger is-fullwidth"  style="width: 100%;">
+											19/02/2021
+										</a>
+										<div class="dropdown-menu drop2">
+											<a class="dropdown-item" href="index.html#17022021-1">
+												Echec TPU, redesign
+											</a>
+										</div>
+									</div>
+
+									<div class="navbar-item dropdown is-hoverable">
+										<a class="navbar-link dropdown-trigger is-fullwidth"  style="width: 100%;">
+											17/02/2021
+										</a>
+										<div class="dropdown-menu drop2">
+											<a class="dropdown-item" href="index.html#17022021-1">
+												Côté et importation des composants électroniques du robot
+											</a>
+										</div>
+									</div>
+
+									<div class="navbar-item dropdown is-hoverable">
+										<a class="navbar-link dropdown-trigger is-fullwidth"  style="width: 100%;">
+											16/02/2021
+										</a>
+										<div class="dropdown-menu drop2">
+											<a class="dropdown-item" href="index.html#16022021-1">
+												Chaîne et roues
+											</a>
+										</div>
+									</div>
+
+									<div class="navbar-item dropdown is-hoverable">
+										<a class="navbar-link dropdown-trigger is-fullwidth"  style="width: 100%;">
+											10/02/2021
+										</a>
+										<div class="dropdown-menu drop2">
+											<a class="dropdown-item" href="index.html#10022021-2">
+												Reconnaissance vocale
+											</a>
+											<a class="dropdown-item" href="index.html#10022021-1">
+												Nom de domaine
+											</a>
+										</div>
+									</div>
+								</div>
+							</div>
+							<a class="navbar-item" href="./#galerie">
+								Galerie
+							</a>
+							<a class="navbar-item" href="./#stats">
+								Statistiques
+							</a>
+							<a class="navbar-item" href="./#contact">
+								Contact
+							</a>
+						</div>
+						<!---
+						<div class="navbar-end">
+							<div class="navbar-item">
+								<div class="buttons">
+									<a class="button is-primary">
+										<strong>Sign up</strong>
+									</a>
+									<a class="button is-light">
+										Log in
+									</a>
+								</div>
+							</div>
+						</div>
+						-->
+					</div>
+				</div>
+			</nav>
+		</div>
