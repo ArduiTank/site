7cab2721c742f20f769a316d986038984cbb018c
Antonin De Breuck <antonindebreuck@gmail.com>
2021-05-28 22:41:31 +0200

    Create erreur_500.php

---
+++ erreurs/erreur_500.php
@@ -0,0 +1,25 @@
+<?php
+$pageTitle = 'Merci d\'actualiser la page.';
+
+include('../include/head.php');
+include('../include/nav.php');
+ ?>
+ <section class="hero is-light" style="padding-top: 100px;">
+   <div class="hero-body is-marginless is-paddingless">
+     <div class="container">
+       <img class="image is-4by4" src="../img/logo_transparent.png" height="300px" width="300px">
+     </div>
+   </div>
+ </section>
+ <section class="hero is-danger">
+   <div class="hero-body">
+     <div class="container">
+       <h1 class="title">
+         Erreur 500
+       </h1>
+       <h2 class="subtitle">
+         Merci d'actualiser la page. Si le problème persiste, contactez un administrateur.
+       </h2>
+     </div>
+   </div>
+ </section>
