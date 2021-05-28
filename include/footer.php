ccb3de12576bf7097ac84772627208868c09f05d
Antonin De Breuck <antonindebreuck@gmail.com>
2021-05-28 21:40:41 +0200

    Update footer.php

--- include/footer.php
+++ include/footer.php
@@ -5,8 +5,22 @@
 						Créé le 10/02/2020
 					</li>
 					<li>
+						Copyright ArduiTank © <span class="year"></span>. All Rights Reserved
+					</li>
+					<li>
 						Par AntoDB
 					</li>
 				</ul>
 			</nav>
 		</div>
+
+		<!-- Copyright -->
+		<script type="text/javascript">
+			function year() {
+				var now = new Date();
+				$('.year').html(now.getFullYear());
+			};
+			$(document).ready(function() {
+				year();
+			});
+		</script>
