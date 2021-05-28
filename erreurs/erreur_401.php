760a2b796a19499f3c4c2cb47c865268a1f0a86e
Antonin De Breuck <antonindebreuck@gmail.com>
2021-05-28 22:41:19 +0200

    Create erreur_401.php

---
+++ erreurs/erreur_401.php
@@ -0,0 +1,25 @@
+<?php
+$pageTitle = 'Accès refusé !';
+
+include('../include/head.php');
+include('../include/nav.php');
+ ?>
+ <section class="hero is-light" style="padding-top: 100px;">
+   <div class="hero-body is-marginless is-paddingless">
+     <div class="container">
+       <img class="image is-4by4" src="../img/logo_transparent.png" height="300px" width="300px">
+     </div>
+   </div>
+ </section>
+ <section class="hero is-danger">
+   <div class="hero-body">
+     <div class="container">
+       <h1 class="title">
+         Erreur 401
+       </h1>
+       <h2 class="subtitle">
+         Merci de vous authentifier pour acceder à cette page.
+       </h2>
+     </div>
+   </div>
+ </section>
