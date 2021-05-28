4ac1073431e843f9be8aa04c8897760182a1a5b2
Antonin De Breuck <antonindebreuck@gmail.com>
2021-05-28 22:41:24 +0200

    Create erreur_403.php

---
+++ erreurs/erreur_403.php
@@ -0,0 +1,25 @@
+<?php
+$pageTitle = 'Accès refusé !';
+
+include('../include/head.php');
+include('../include/nav.php');
+ ?>
+ <section class="hero is-light" style="padding-top: 100px;">
+   <div class="hero-body is-marginless is-paddingless">
+     <div class="container">
+       <img class="image is-4by4" src="../img/logo_transparent.png" height="300px" width="300px">
+     </div>
+   </div>
+ </section>
+ <section class="hero is-danger">
+   <div class="hero-body">
+     <div class="container">
+       <h1 class="title">
+         Erreur 403
+       </h1>
+       <h2 class="subtitle">
+         L'accès à cette page vous à été refusé. Si vous pensez que c'est une erreur, merci de contacter un administrateur.
+       </h2>
+     </div>
+   </div>
+ </section>
