85889a287835b6f18750b7ca308216c61b7bc47a
Antonin De Breuck <antonindebreuck@gmail.com>
2021-05-28 22:48:04 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,56 +1,9 @@
 <!DOCTYPE html>
 <html lang="fr" style="overflow-y: auto; scroll-behavior: smooth;">
 <head>
-	<meta charset='utf-8'>
-	<meta http-equiv='X-UA-Compatible' content='IE=edge'>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<title>ArduiTank | Accueil</title>
-	<meta itemprop="name" content="ArduiTank | Accueil" />
-	<meta name="keywords" content="ArduiTank, Arduino, DIY, bois, 3D, modélisation, ISAT, EPHEC, AntoDB"/>
-
-	<link rel="apple-touch-icon" type="image/png" sizes="180x180" href="./img/Logo V2.2 min.png" class="next-head">
-	<link rel="icon" type="image/png" sizes="32x32" href="./img/Logo V2.2 min.png" class="next-head">
-	<link rel="icon" type="image/png" sizes="16x16" href="./img/Logo V2.2 min.png" class="next-head">
-
-	<link rel="canonical" href="https://arduitank.be" />
-	<link rel="shortlink" href="https://arduitank.be" />
-
-	<meta name="viewport" content="width=device-width, initial-scale=1" class="next-head">
-	<meta name="description" content="Bienvenue sur le site d'ArduiTank. Equipe de projet pour char robotisé et télécommandé." class="next-head">
-
-	<meta property="og:title" content="ArduiTank | Accueil" class="next-head"/>
-	<meta property="og:site_name" content="ArduiTank | Accueil" class="next-head" />
-	<meta property="og:type" content="website" class="next-head" />
-	<meta property="og:url" content="https://arduitank.be" class="next-head" />
-	<meta property="og:description" content="Bienvenue sur le site d'ArduiTank. Equipe de projet pour char robotisé et télécommandé.">
-	<meta property="og:image" content="http://arduitank.be/img/logo%20V2.2.png" class="next-head">
-
-	<meta name="twitter:card" content="summary_large_image" class="next-head" />
-	<meta name="twitter:title" content="ArduiTank | Accueil" class="next-head" />
-	<meta name="twitter:url" content="https://arduitank.be" />
-	<meta name="twitter:description" content="Bienvenue sur le site d'ArduiTank. Equipe de projet pour char robotisé et télécommandé." class="next-head">
-	<meta name="twitter:creator" content="@ArduiTank" class="next-head">
-	<meta name="twitter:image:src" content="http://arduitank.be/img/logo%20V2.2.png" class="next-head">
-
-	<meta name="dcterms.title" content="ArduiTank | Accueil" class="next-head" />
-	<meta name="dcterms.type" content="Text" class="next-head" />
-	<meta name="dcterms.format" content="text/html" class="next-head" />
-	<meta name="dcterms.identifier" content="https://arduitank.be" class="next-head" />
-	<meta name="dcterms.description" content="Bienvenue sur le site d'ArduiTank. Equipe de projet pour char robotisé et télécommandé." class="next-head">
-	
-	<!-- Bulma Version 0.7.4-->
-	<link rel="stylesheet" href="https://unpkg.com/bulma@0.7.4/css/bulma.min.css" />
-	<link rel="stylesheet" href="https://unpkg.com/bulma-modal-fx/dist/css/modal-fx.min.css" />
-	
-	<!-- Bootstrap Version 5.0.0 beta 1 -->
-	<!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">-->
-
-	<!-- Fonts | Emotes -->
-	<script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
-
-	<!-- For the time manage -->
-	<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+	<?php
+		include("include/navbar.php");
+	?>
 
 	<style>
 		/* Image for background */
