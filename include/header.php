b823e799a49d2ca50b6b44f08bc83b59fe303005
Antonin De Breuck <antonindebreuck@gmail.com>
2021-05-28 02:06:27 +0200

    Update header.php

--- include/header.php
+++ include/header.php
@@ -251,6 +251,9 @@
 							<a class="navbar-item" href="./#contact">
 								Contact
 							</a>
+							<a class="navbar-item has-text-white" href="https://github.com/ArduiTank" target="_blank">
+								<i class="fab fa-github" aria-hidden="true" style="width: 1.5em; height: auto;"></i>
+							</a>
 							<a class="navbar-item has-text-white" href="https://discord.gg/D77Vxmehk7" target="_blank">
 								<i class="fab fa-discord" aria-hidden="true" style="width: 1.5em; height: auto;"></i>
 							</a>
