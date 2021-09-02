b0df89e051c0ffd76f7ff313ba1c69589ff2aea4
Antonin De Breuck <antonindebreuck@gmail.com>
2021-09-03 01:19:24 +0200

    Update : Avatar Discord Ugo

--- index.php
+++ index.php
@@ -267,7 +267,7 @@
 					<div class="column">
 						<center style="margin-bottom: 1rem;">
 							<figure class="image is-64x64">
-								<img src="https://cdn.discordapp.com/avatars/693118212711251968/c687df4818f2f0a24ebed85ced3d0bb5.png" class="image is-64x64 is-rounded">
+								<img src="https://cdn.discordapp.com/avatars/693118212711251968/e5a03db2de4e163557c21cf59a07bae3.png" class="image is-64x64 is-rounded">
 							</figure>
 							<br/>
 							<p class="title is-5" style="font-weight: normal;">
